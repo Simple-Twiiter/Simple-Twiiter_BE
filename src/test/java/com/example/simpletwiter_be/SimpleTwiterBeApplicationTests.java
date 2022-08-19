a5437ef5d6acc7d844d21cc8f8c327a0da4a7fb6
zmfkzj <qlwlal@naver.com>
2022-08-19 16:41:57 +0900

    create repository

---
+++ src/test/java/com/example/simpletwiter_be/SimpleTwiterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpletwiter_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTwiterBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
