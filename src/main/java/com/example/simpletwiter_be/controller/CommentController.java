b00ce354062ac39f3ce5fe5bbdfd95df6196663f
sungho-kim <qkrkdi1124@naver.com>
2022-08-19 17:26:05 +0900

    comment 파일

---
+++ src/main/java/com/example/simpletwiter_be/controller/CommentController.java
@@ -0,0 +1,4 @@
+package com.example.simpletwiter_be.controller;
+
+public class CommentController {
+}
---
+++ src/main/java/com/example/simpletwiter_be/dto/response/ResponseDto.java
@@ -0,0 +1,5 @@
+package com.example.simpletwiter_be.dto.response;
+
+public class ResponseDto {
+
+}
