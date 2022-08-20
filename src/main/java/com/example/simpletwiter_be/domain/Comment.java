dfaf7539e72e8fad1ae166c8b31d0ac186d507a7
sungho-kim <qkrkdi1124@naver.com>
2022-08-20 20:25:31 +0900

    Comment 일부/토큰

--- src/main/java/com/example/simpletwiter_be/domain/Comment.java
+++ src/main/java/com/example/simpletwiter_be/domain/Comment.java
@@ -1,4 +1,40 @@
 package com.example.simpletwiter_be.domain;
 
-public class Comment {
+import com.example.simpletwiter_be.dto.request.CommentRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @JoinColumn(name = "member_id",nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "post_id",nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+//    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "comment")
+//    private List<CommentLike> commentLikes;
+
+    public void update(CommentRequestDto commentRequestDto){this.content = commentRequestDto.getContent();}
+
+
 }
