1a2551d8d65afc3f16282e1d70a0f9568cfb0398
zmfkzj <qlwlal@naver.com>
2022-08-24 03:22:20 +0900

    [minkyu] <feat> image upload 개발 완료, 리펙토링 조금 함.

--- src/main/java/com/example/simpletwiter_be/repository/MemberRepository.java
+++ src/main/java/com/example/simpletwiter_be/repository/MemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Optional<Member> findById(Long id);
-    Optional<Member> findByUsername(String username);
+    Optional<Member> findByUsername(String email);
+    boolean existsByUsername(String email);
 }
