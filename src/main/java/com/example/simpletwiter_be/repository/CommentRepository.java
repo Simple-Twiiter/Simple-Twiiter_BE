0102339bb1d3ccc27196780d52cc01fef0b5207d
sungho-kim <qkrkdi1124@naver.com>
2022-08-23 21:00:25 +0900

    김성호<feat> comment 1차 완성

--- src/main/java/com/example/simpletwiter_be/repository/CommentRepository.java
+++ src/main/java/com/example/simpletwiter_be/repository/CommentRepository.java
@@ -3,14 +3,15 @@ package com.example.simpletwiter_be.repository;
 import com.example.simpletwiter_be.domain.Comment;
 import com.example.simpletwiter_be.domain.Member;
 import com.example.simpletwiter_be.domain.Post;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByPost(Post post);
-    List<Comment> findByMember(Member member);
+    List<Comment> findAllByPost(Post post, PageRequest pageRequest);
+//    List<Comment> findByMember(Member member);
 
 
 }
