fcf458e060085d669e3aa7a923e00906df1eeca7
zmfkzj <qlwlal@naver.com>
2022-08-24 21:59:03 +0900

    [minkyu] <fix> getPost는 로그인 안해도 가능하도록 수정

--- src/main/java/com/example/simpletwiter_be/shared/UserGetterFromToken.java
+++ src/main/java/com/example/simpletwiter_be/shared/UserGetterFromToken.java
@@ -16,21 +16,26 @@ import java.util.function.Supplier;
 public class UserGetterFromToken {
     private final TokenProvider tokenProvider;
 
-    public ResponseDto<?> applyPostService(HttpServletRequest request, Function<Member,ResponseDto<?>> fn){
+    public ResponseDto<?> applyPostService(HttpServletRequest request, Function<Member,ResponseDto<?>> fn, boolean requestAuth){
         if (null == request.getHeader("RefreshToken")) {
-//            return new Result(null, "로그인이 필요합니다.");
             return ResponseDto.fail("123로그인이 필요합니다.");
+        } else if (!requestAuth) {
+            return fn.apply(null);
         }
 
         if (null == request.getHeader("Authorization")) {
-//            return new Result(null, "로그인이 필요합니다.");
             return ResponseDto.fail("456로그인이 필요합니다.");
+        } else if (!requestAuth) {
+            return fn.apply(null);
         }
 
         Member member = validateMember(request);
         if (null == member) {
-//            return new Result(null, "Token이 유효하지 않습니다.");
-            return ResponseDto.fail("Token이 유효하지 않습니다.");
+            if (!requestAuth){
+                return fn.apply(member);
+            }else{
+                return ResponseDto.fail("Token이 유효하지 않습니다.");
+            }
         }
         return fn.apply(member);
 
